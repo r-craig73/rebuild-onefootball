ab5ce40db5e70e42b82b0774a6db6d73d0ec4a58
Ron Craig <ron.craig@comcast.net>
2018-04-07 16:11:39 -0700

    add json file and ts articles, edit article arguments, news styling

--- src/app/article.model.ts
+++ src/app/article.model.ts
@@ -1,4 +1,4 @@
 export class Article {
-  constructor (public title: string, public description: string, public source: string, public published: string, public imagePath: string) {}
-  
+  constructor (public title: string, public description: string, public source: string, public published: string, public imagePath: string, public id: string) {}
+
 }
